bbbdc2d8c51819aa295ec7e0a924048a4eb1be90
Arnav Gupta <championswimmer@gmail.com>
2023-06-06 03:08:28 +0530

    write Netlify cloud function
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ netlify/edge-functions/grabatar.ts
@@ -0,0 +1,31 @@
+import type { Config, Context } from "https://edge.netlify.com";
+import { Hash, encode } from "https://deno.land/x/checksum@1.2.0/mod.ts";
+
+export default (request: Request, context: Context) => {
+
+  const reqURL = new URL(request.url);
+  const { pathname } = reqURL;
+  const [, email] = pathname.split("/");
+  // Create MD5 hash of email
+  const emailHash = new Hash("md5").digestString(email).hex()
+
+  return new Response(`
+    <html>
+      <head>
+        <title>Grabatar</title>
+      </head>
+      <body>
+        <img src="https://www.libravatar.org/avatar/${emailHash}?s=512" />
+      </body>
+    </html>
+  `, {
+    headers: {
+      "content-type": "text/html; charset=UTF-8",
+    }
+  })
+
+};
+
+export const config = {
+  path: "/*"
+}
